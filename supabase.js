380caaa72b42c54bf34128bcee6dfc6d861d668b
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-16 10:48:20 -0300

    Update supabase.js
    reset supabase file

--- supabase.js
+++ supabase.js
@@ -4,6 +4,6 @@ const SUPABASE_URL = ''
 const SUPABASE_ANON_KEY = ''
 
 export const supabase = createClient(
-  https://qdgsmnfhpbkbovptwwjp.supabase.co,
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZ3NtbmZocGJrYm92cHR3d2pwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODE1NDUzNiwiZXhwIjoyMDgzNzMwNTM2fQ.W5iFRYrGVhgc9ySXdBgl00A1K1lHyjAz5ZTIP7C4ZrA)
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 )
