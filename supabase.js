3001806265de7faf355790494bd8d42fd07609ae
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-16 10:49:46 -0300

    Update supabase.js
    add supabase keys correctly

--- supabase.js
+++ supabase.js
@@ -4,6 +4,6 @@ const SUPABASE_URL = ''
 const SUPABASE_ANON_KEY = ''
 
 export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  const SUPABASE_URL = 'https://qdgsmnfhpbkbovptwwjp.supabase.co',
+  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZ3NtbmZocGJrYm92cHR3d2pwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODE1NDUzNiwiZXhwIjoyMDgzNzMwNTM2fQ.W5iFRYrGVhgc9ySXdBgl00A1K1lHyjAz5ZTIP7C4ZrA)'
 )
