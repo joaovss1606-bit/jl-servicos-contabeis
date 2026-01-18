690afd1089781b576d69fdb3d5e68c8a642ddb91
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-18 10:20:02 -0300

    Atualizar o pessoa.js

--- servicos/pessoa-fisica/pessoa.js
+++ servicos/pessoa-fisica/pessoa.js
@@ -1,1 +1,24 @@
-hajs
+const servicosPessoaFisica = {
+  'imposto-renda': {
+    titulo: 'Declaração de Imposto de Renda',
+    descricao: 'Elaboração e envio da declaração anual de IRPF.',
+    valor: 'R$ 180,00'
+  },
+  'ganho-capital': {
+    titulo: 'Apuração de Ganho de Capital',
+    descricao: 'Cálculo de imposto sobre venda de bens.',
+    valor: 'R$ 250,00'
+  },
+  'regularizacao-cpf': {
+    titulo: 'Regularização de CPF',
+    descricao: 'Correção de pendências e situação cadastral.',
+    valor: 'R$ 120,00'
+  },
+  'orientacao-fiscal': {
+    titulo: 'Orientação Fiscal Personalizada',
+    descricao: 'Atendimento individual para dúvidas fiscais.',
+    valor: 'R$ 150,00'
+  }
+}
+
+window.servicosCategoria = servicosPessoaFisica
