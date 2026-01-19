be5437a107864db92f0ec085aa889f09d000b9b4
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-19 16:44:37 -0300

    Update contabeis.js

--- servicos/contabeis/contabeis.js
+++ servicos/contabeis/contabeis.js
@@ -2,17 +2,17 @@ const servicosContabeis = {
   'consultoria-contabil': {
     titulo: 'Consultoria Contábil',
     descricao: 'Análise e orientação contábil especializada.',
-    valor: 'R$ 250,00'
+    valor: 'R$ 249,99'
   },
   'planejamento-tributario': {
     titulo: 'Planejamento Tributário',
     descricao: 'Estratégias para redução legal de impostos.',
-    valor: 'R$ 350,00'
+    valor: 'R$ 399,99'
   },
   'balanco': {
     titulo: 'Elaboração de Balanço',
     descricao: 'Balanço patrimonial e demonstrativos.',
-    valor: 'R$ 300,00'
+    valor: 'R$ 349,99'
   }
 }
 
